7e2a43ae3ab821926f6d8a2a9e2d61c1ea58b887
Gonzalo Rodríguez Jurado <gonzalorodjur01@gmail.com>
2024-03-25 14:45:46 +0100

    upd bonus

---
+++ ft_lstnew.c
@@ -0,0 +1,12 @@
+#include "libft.h"
+
+t_list *ft_lstnew(void *content)
+{
+	t_list *new_node;
+	new_node = malloc(sizeof(content));
+	if (!new_node)
+		return (NULL);
+	new_node->content = content;
+	new_node->next = NULL;
+	return (new_node);
+}
